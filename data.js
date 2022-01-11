8c166686d0588d3cd6ed90e04e159ca6818c4e80
Paul Cooper <paulcooper@Pauls-MacBook-Air.local>
2022-01-11 19:14:21 +0000

    data.js added

---
+++ data.js
@@ -0,0 +1,86 @@
+const candidates = [
+    {
+        id: "LABOUR",
+        name: "Ben Wood",
+        party: "LABOUR PARTY",
+        votes: 0,
+    },
+    {
+        id: "CONSERVATIVE",
+        name: "Dr. Neil Shastri-Hurst",
+        party: "CONSERVATIVE PARTY",
+        votes: 0,
+    },
+    {
+        id: "LIBERAL_DEMOCRAT",
+        name: "Helen Morgan",
+        party: "LIBERAL DEMOCRAT PARTY",
+        votes: 0,
+    },
+    {
+        id: "GREEN",
+        name: "Duncan Kerr",
+        party: "GREEN PARTY",
+        votes: 0,
+    },
+    {
+        id: "REFORM_PARTY",
+        name: "Kirsty Walmsley",
+        party: "REFORM PARTY",
+        votes: 0,
+    },
+    {
+        id: "PATIENT_SUPPORT_TRUST",
+        name: "Yolande Kenward",
+        party: "PATIENT SUPPORT TRUST PARTY",
+        votes: 0,
+    },
+    {
+        id: "FREEDOM_ALLIANCE",
+        name: "Earl Jesse",
+        party: "FREEDOM ALLIANCE PARTY",
+        votes: 0,
+    },
+    {
+        id: "OFFICIAL_MONSTER_RAVING_LOONY_PARTY",
+        name: "Alan 'Howling Laud' Hope",
+        party: "OFFICIAL MONSTER RAVING LOONY PARTY",
+        votes: 0,
+    },
+    {
+        id: "HERITAGE_PARTY",
+        name: "James Elliott",
+        party: "HERITAGE PARTY",
+        votes: 0,
+    },
+    {
+        id: "THE_PARTY_PARTY",
+        name: "Russell Dean",
+        party: "THE PARTY PARTY",
+        votes: 0,
+    },
+    {
+        id: "RECLAIM_PARTY",
+        name: "Martin Daubney",
+        party: "RECLAIM PARTY",
+        votes: 0,
+    },
+    {
+        id: "REJOIN_EU",
+        name: "Boris Been-Bunged",
+        party: "REJOIN EU PARTY",
+        votes: 0,
+    },
+    {
+        id: "UKIP",
+        name: "Andrea Allen",
+        party: "UNITED KINGDOM INDEPENDENCE PARTY",
+        votes: 0,
+    },
+    {
+        id: "INDEPENDENT",
+        name: "Suzie Akers-Smith",
+        party: "INDEPENDENT",
+        votes: 0,
+    },
+]
